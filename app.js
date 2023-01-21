3cbd2883d291cb0c95d9021dc1d226427c7c9cad
0-AB <122486093+0-AB@users.noreply.github.com>
2023-01-21 04:09:15 -0800

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const fs = require('fs')
 const port = 3000
-
+const app = express()
 app.get('/', (req, res) => {
     const {url} = req.query
     const readable = fs.createReadStream(url)
